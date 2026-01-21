b6281106bd6179f8952dde817dfedd22c62d2388
Davide Barbagallo <barbang.davide@gmail.com>
2026-01-21 12:27:48 +0100

    feat: add CORS

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
@@ -6,8 +7,19 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  const configService = app.get(ConfigService)
+
+  // CORS Configuration
+  const corsOrigin = configService.get<string>('CORS_ORIGIN', '*')
+  app.enableCors({
+    origin: corsOrigin,
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+  })
+
+  // Validation Pipe Configuration
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
 
+  // Swagger Configuration
   const config = new DocumentBuilder().setTitle('Trip Manager').build()
   const documentFactory = () => SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('docs', app, documentFactory)
@@ -15,4 +27,4 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000)
 }
 
-bootstrap()
+void bootstrap()
