03b7ebaadc715a1da6a165b20b6074c089c4983c
Davide Barbagallo <barbang.davide@gmail.com>
2026-01-22 10:42:07 +0100

    perf: remove useless env variable check

--- src/providers/trips-api/trips-api.service.ts
+++ src/providers/trips-api/trips-api.service.ts
@@ -10,9 +10,7 @@ export class TripsApiService {
   constructor(private readonly configService: ConfigService) {
     const baseUrl = this.configService.getOrThrow<string>('API_BASE_URL')
     const apiKey = this.configService.getOrThrow<string>('API_KEY')
-    if (!apiKey) {
-      throw new Error('API_KEY was not provided in configuration file')
-    }
+
     this.httpClientInstance = axios.create({
       baseURL: baseUrl,
       headers: { 'x-api-key': apiKey },
